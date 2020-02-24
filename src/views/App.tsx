af033e0c63532020f8cf8856a2e6eb9de4ce6949
Randy Nghiem <randy.nghiem@hotmail.com>
2020-02-24 12:17:41 +0000

    add ant design

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from "antd";
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      Todo
+      <Button type="primary">Button</Button>
     </div>
   );
 }
